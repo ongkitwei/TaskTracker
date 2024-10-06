d43c6e75a7fcad7a871c26efcac61c0c99d42cc2
ong kit wei <ongkw@ongs-MacBook-Pro.local>
2024-10-06 15:48:37 +0800

    rendering done, added colors to diff priority

--- src/ContentPage.jsx
+++ src/ContentPage.jsx
@@ -11,17 +11,17 @@ function ContentPage() {
   const [showModal, setModal] = useState(false);
 
   // state for Modal.jsx
-  const[todosTitle, settodosTitle] = useState(["TEST TITLE"]);
-  const[todosDescrip, settodosDescrip] = useState(["sample description"]);
-  const[todosPior, settodosPior] = useState(["High"]);
+  const[todosTitle, settodosTitle] = useState([]);
+  const[todosDescrip, settodosDescrip] = useState([]);
+  const[todosPior, settodosPior] = useState([]);
 
   return (
     <MyContext.Provider value={{todosTitle, settodosTitle, todosDescrip, settodosDescrip, todosPior, settodosPior}}>
-    <div className="mt-[60px] ml-[140px] flex flex-col justify-start w-full">
+    <div className="mt-[60px] ml-[140px] mr-[140px] flex flex-col justify-start w-full">
         <div className="border-b-2 border-gray-150 pb-5">
             <header className="text-2xl">To-Do</header>
         </div>
-        <div className="pt-4 flex flex-row">
+        <div className="pt-4 pb-2 flex flex-row">
           <button className="bg-blue-700 text-white py-[2px] px-[15px] rounded-lg hover:bg-blue-600" onClick={() => setModal(true)}>+ New Task</button>
           <button className="flex items-center bg-white ml-4 border-2 border-gray-150 rounded-lg px-[10px] py-[2px]">
             <FiFilter className="mr-[10px]"></FiFilter>
--- src/TodoTask.jsx
+++ src/TodoTask.jsx
@@ -1,13 +1,36 @@
 import React, { useContext } from 'react';
+import { MyContext } from './contentPage';
 
 function TodoTask() {
+  const {
+    todosTitle, settodosTitle, 
+    todosDescrip, settodosDescrip, 
+    todosPior, settodosPior
+  } = useContext(MyContext);
+
   return (
-    <div className="border-2 border-blue-400 mt-3 rounded-lg p-2">
-        <input type="checkbox"></input>
-        <span>sadasdsd</span>
-        <p>details...</p>
+    <div>
+      {todosTitle.map((todo, index) => (
+      <div className="border-2 border-gray-300 mt-3 rounded-lg p-2 flex flex-row items-center justify-between">
+        <div className="flex flex-col">
+            <div className="flex items-center">
+              <input type="checkbox" className="mr-1"/>
+              <header className="font-semibold text-lg">{todo}</header>
+            </div>
+            <div>
+              <i className="pl-4 text-gray-400 text-[15px]">{todosDescrip[index]}</i>
+            </div>
+          </div>
+          <div>
+            <button className={`border-2 p-1 rounded-lg ${todosPior[index]=="High" ? " border-red-400 text-red-400 bg-red-100" 
+              : todosPior[index]=="Medium" ? "border-orange-400 text-orange-400 bg-orange-100" 
+              : "border-yellow-400 text-yellow-400 bg-yellow-100"}`}>{todosPior[index]}</button>
+        </div>        
+      </div>
+      ))}
     </div>
-  )
+
+  );
 }
 
 export default TodoTask;
