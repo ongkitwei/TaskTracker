aeef173587f82e8b724ea2f53c1cda7b7164f916
ong kit wei <ongkw@ongs-MacBook-Pro.local>
2024-10-06 20:21:24 +0800

    change img to react-icon

--- src/SideBar.jsx
+++ src/SideBar.jsx
@@ -1,14 +1,15 @@
-import DashboardIcon from "./assets/dashboard-icon.png";
-import WelcomeIcon from  "./assets/welcome-icon.png";
-import TaskIcon from "./assets/task-icon.png";
-import PeopleIcon from "./assets/people-icon.png";
-import ReportIcon from "./assets/reports-icon.png";
-import BillingIcon from "./assets/billing-icon.png";
-import IntegrationIcon from "./assets/integration-icon.png";
+import { PiSmileyLight } from "react-icons/pi";
+import { AiOutlineDashboard } from "react-icons/ai";
+import { MdOutlineTask } from "react-icons/md";
+import { BsPeople } from "react-icons/bs";
+import { BsGraphUpArrow } from "react-icons/bs";
+import { AiOutlineDollar } from "react-icons/ai";
+import { GrIntegration } from "react-icons/gr";
+
 
 function SideBar() {
     const sideBarContent = ["Welcome", "Dashboard", "Tasks", "People", "Reports", "Billing", "Integrations"];
-    const sideBarImg = [WelcomeIcon, DashboardIcon, TaskIcon, PeopleIcon, ReportIcon, BillingIcon, IntegrationIcon];
+    const sideBarImg = [<PiSmileyLight/>, <AiOutlineDashboard />, <MdOutlineTask/>, <BsPeople/>, <BsGraphUpArrow/>, <AiOutlineDollar/>, <GrIntegration/>];
     const sideBarEventHandler = [welcomePage, dashboardPage, tasksPage, peoplePage, reportsPage, billingPage, integrationsPage];
 
     function welcomePage(){
@@ -43,8 +44,8 @@ function SideBar() {
         </div>
         <div className="flex flex-col ml-5 mr-30 border-t-2 border-gray-150 pt-6">
             {sideBarContent.map((item, index) => 
-            <a className="flex pb-[25px] hover: cursor-pointer" key={index} onClick={sideBarEventHandler[index]}>
-                <img className="w-5 mr-2" src={sideBarImg[index]}></img>
+            <a className="flex pb-[25px] hover: cursor-pointer items-center" key={index} onClick={sideBarEventHandler[index]}>
+                <div className="mr-2">{sideBarImg[index]}</div>
                 {item}
             </a>
         )} 
